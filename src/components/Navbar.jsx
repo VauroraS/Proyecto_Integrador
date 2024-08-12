ca6215d066edd29d954b98a73d78b58a13e9e0d6
Vanina <55039948+VauroraS@users.noreply.github.com>
2024-08-11 22:45:27 -0300

    importar-logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
+import logo from '../assets/pez.png'; // Importa la imagen
 
 const Navbar = () => {
   const { user, logout } = useContext(AuthContext);
@@ -10,7 +11,7 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center">
         <Link className="navbar-brand navbar-logo flex items-center" to="/">
           <img
-            src="../src/assets/pez.png"
+            src={logo} // Usa la variable `logo` en lugar de la ruta
             alt="Logo"
             className="w-14 h-14 md:w-22 md:h-22 lg:w-26 lg:h-26"
           />
